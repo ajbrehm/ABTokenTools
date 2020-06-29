62bc04878defdbf416c0533218a787828cbd457b
Andrew J. Brehm <andrew@netneurotic.net>
2020-06-29 15:03:01 +0200

    added copyasbackup

---
+++ CopyAsBackup/CopyAsBackup.c
@@ -0,0 +1,57 @@
+//MIT License
+//
+//Copyright(c) 2020 Andrew J. Brehm
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this softwareand associated documentation files(the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions :
+//
+//The above copyright noticeand this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+#include <Windows.h>
+#include <stdio.h>
+#define BUFFER_SIZE 256
+
+int main(int argc, LPTSTR argv[])
+{
+	HANDLE hIn, hOut;
+	DWORD in, out;
+	CHAR aBuffer[BUFFER_SIZE];
+
+	if (argc != 3) {
+		printf("CopyAsBackup pathSource pathTarget\n");
+		return 1;
+	}//if
+
+	hIn = CreateFileA(argv[1], GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
+	if (INVALID_HANDLE_VALUE == hIn) {
+		printf("Cannot open input file. Error %x\n", GetLastError());
+		return 2;
+	}//if
+
+	hOut = CreateFileA(argv[2], GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (INVALID_HANDLE_VALUE == hOut) {
+		printf("Cannot open output file. Error: %x\n", GetLastError());
+		return 3;
+	}//if
+
+	while (ReadFile(hIn, aBuffer, BUFFER_SIZE, &in, NULL) && in > 0) {
+		WriteFile(hOut, aBuffer, in, &out, NULL);
+		if (in != out) {
+			printf("Fatal write error: %x\n", GetLastError());
+			return 4;
+		}//if
+	}//while
+}
