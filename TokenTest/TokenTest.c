ce48db33729035aa73e4fd43b4e90d28949172ff
Andrew J. Brehm <andrew@netneurotic.net>
2020-10-03 22:55:52 +0200

    continued

--- TokenTest/TokenTest.c
+++ TokenTest/TokenTest.c
@@ -1,6 +1,8 @@
 #include <Windows.h>
 #include <securitybaseapi.h>
 #include <processthreadsapi.h>
+#include <NTSecAPI.h>
+#include <security.h>
 
 HANDLE hOut;
 DWORD error;
@@ -43,8 +45,63 @@ void ConfigureCommandLine()
 int main()
 {
 	ConfigureCommandLine();
-
+	
 	HANDLE hToken = GetCurrentProcessToken();
+
+	if (2 == args) {
+
+		LPWSTR sUserName = aCommandLine[1];
+		
+		HANDLE hLsaConnection;
+		ok = LsaConnectUntrusted(&hLsaConnection);
+
+		//NTSTATUS LsaLogonUser(
+		//	HANDLE              LsaHandle,
+		//	PLSA_STRING         OriginName,
+		//	SECURITY_LOGON_TYPE LogonType,
+		//	ULONG               AuthenticationPackage,
+		//	PVOID               AuthenticationInformation,
+		//	ULONG               AuthenticationInformationLength,
+		//	PTOKEN_GROUPS       LocalGroups,
+		//	PTOKEN_SOURCE       SourceContext,
+		//	PVOID * ProfileBuffer,
+		//	PULONG              ProfileBufferLength,
+		//	PLUID               LogonId,
+		//	PHANDLE             Token,
+		//	PQUOTA_LIMITS       Quotas,
+		//	PNTSTATUS           SubStatus
+		//);
+
+		// prepare an origin
+		LSA_STRING lsaOrigin;
+		lsaOrigin.Buffer = L"TokenTest";
+		lsaOrigin.Length = wcslen(lsaOrigin.Buffer) * sizeof(WCHAR) + sizeof(WCHAR);
+
+		// prepare authentication package
+		ULONG authenticationpackage = 0;
+		LSA_STRING lsaAuthenticationPackage;
+		lsaAuthenticationPackage.Buffer = NEGOSSP_NAME_W;
+		lsaAuthenticationPackage.Length = (USHORT)wcslen(lsaAuthenticationPackage.Buffer);
+		lsaAuthenticationPackage.MaximumLength = lsaAuthenticationPackage.MaximumLength;
+		ok = LsaLookupAuthenticationPackage(hLsaConnection, &lsaAuthenticationPackage, &authenticationpackage);
+		
+		// prepare authentication info
+		ULONG cchUserName = wcslen(sUserName);
+		ULONG cbUserName = cchUserName * sizeof(WCHAR) + sizeof(WCHAR);
+		ULONG cbAuthenticationInfo = sizeof(MSV1_0_S4U_LOGON) + cbUserName;
+		UNICODE_STRING unisUserName;
+		unisUserName.Buffer = sUserName;
+		unisUserName.Length = cbUserName;
+		MSV1_0_S4U_LOGON logon;
+		logon.Flags = MSV1_0_S4U_LOGON_FLAG_CHECK_LOGONHOURS;
+		logon.MessageType = MsV1_0S4ULogon;
+		logon.UserPrincipalName = unisUserName;
+
+		// prepare a token source
+		TOKEN_SOURCE source;
+		
+
+	}//if
 
 	DWORD tilength = 0;
 	ok = GetTokenInformation(hToken, TokenPrivileges, NULL, 0, &tilength);
@@ -64,7 +121,7 @@ int main()
 		LUID luid = luidaa.Luid;
 		DWORD cchName = 0;
 		ok = LookupPrivilegeNameW(NULL, &luid, NULL, &cchName);
-		size_t cbName = (cchName + 1) * sizeof(WCHAR);
+		size_t cbName = cchName * sizeof(WCHAR) + sizeof(WCHAR);
 		LPWSTR sName = (LPWSTR)GlobalAlloc(0, cbName);
 		ok = LookupPrivilegeNameW(NULL, &luid, sName, &cchName);
 		WriteLine(sName);
