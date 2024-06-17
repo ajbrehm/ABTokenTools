7bc246cd50bd483fab36ebcb02d94a00489d924f
Andrew Brehm <ajbrehm@mac.com>
2024-06-17 22:19:49 +0200

    fixed help

--- RunJob/RunJob.c
+++ RunJob/RunJob.c
@@ -133,9 +133,11 @@ void SetWindowStationSecurity(PSID pSid)
 void Help()
 {
 	wprintf(L"\n#0: RunJob /PId pid /JobProcessLimit limit (appplies quota to running process)\n\n");
-	wprintf(L"#1: RunJob /Image pathImage [/JobProcessLimit limit] [[[/Domain sDomain] /User sUser] /Password sPassword] [/SessionId id] [/args ...] (creates a process with various attributes)\n\n");
-	wprintf(L"#2: RunJob /Image pathImage /UseRunAs [/args ...] (spawns a process using RunAs verb)\n\n");
-	wprintf(L"#3: RunJob /WindowStationPermission [/Domain sDomain] /User sUser (allows user access to session window station, use before #1)\n\n");
+	wprintf(L"#1: RunJob /Image pathImage [/JobProcessLimit limit] [[[/Domain sDomain] /User sUser] /Password sPassword] [/args ...] (creates a process with various attributes)\n\n");
+	wprintf(L"#2: RunJob /Image pathImage [/JobProcessLimit limit] [/Domain sDomain] /User sUser [/Password sPassword] /SessionId sessionid [/args ...] (creates a process in another session)\n\n");
+	wprintf(L"#3: RunJob /Image pathImage [/JobProcessLimit limit] /SessionId sessionid [/args ...] (creates a process in another session as that session's user)\n\n");
+	wprintf(L"#4: RunJob /Image pathImage /UseRunAs [/args ...] (spawns a process using RunAs verb)\n\n");
+	wprintf(L"#5: RunJob /WindowStationPermission [/Domain sDomain] /User sUser(allows user access to session window station, use before #2)\n\n");
 	exit(0);
 }
 
@@ -363,18 +365,17 @@ int main()
 				EnablePrivilege(L"SeIncreaseQuotaPrivilege");
 				EnablePrivilege(L"SeAssignPrimaryTokenPrivilege");
 
-				ok = CreateProcessAsUserW(hToken, pathImage, sNewCmdLine, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
+				ok = CreateProcessAsUserW(hToken, pathImage, sNewCmdLine, NULL, NULL, FALSE, dwCreationFlags, NULL, NULL, &si, &pi);
 				Error(L"CreateProcessAsUserW");
 
-				CloseHandle(hToken);
-				exit(0);
 			} else {
 				ok = WTSQueryUserToken(sessionid, &hToken);
 				Error(L"WTSQueryUserToken");
 				CreateProcessAsUserW(hToken, pathImage, sNewCmdLine, NULL, NULL, FALSE, dwCreationFlags, NULL, NULL, &si, &pi);
 				Error(L"CreateProcessAsUserW");
-				CloseHandle(hToken);
+
 			}//if
+			CloseHandle(hToken);
 		} else {
 			if (3 == cDomainUserPassword) {
 				ok = CreateProcessWithLogonW(sUser, sDomain, sPassword, 0, pathImage, sNewCmdLine, dwCreationFlags, NULL, NULL, &si, &pi);
